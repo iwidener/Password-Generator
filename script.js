55c071fe8005a0dd35ced808216ee4293e438984
Inna Widener <iwide2019@gmail.com>
2020-01-09 17:50:30 -0600

    Initial Repository

---
+++ script.js
@@ -0,0 +1,24 @@
+alert("Choose a length of password between 8 and 128 numbers.")
+
+
+function generate(){
+    var complexity = document.getElementById("length").value;
+
+    var values = "ABDCEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789#$%&'()*+,/:;<=>?[\]^_`{|}!~";
+   
+    var password = "";
+
+    for(var i = 1; i <= complexity; i++){
+        password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length + 1)));
+    }
+
+    document.getElementById("display").value = password;
+}
+
+
+function copy(){
+
+    document.getElementById("display").select();
+
+    document.execCommand("copy");
+}
